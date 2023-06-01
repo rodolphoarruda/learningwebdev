350f48f361ada8a598cf97272c84bd6e2a216171
Rodolpho <email@rodolphoarruda.pro.br>
2023-05-31 22:25:35 -0300

    first Lua file

---
+++ web-projects/lua/file.lua
@@ -0,0 +1,18 @@
+--[[ 
+     this is my first file in the Lua programming language
+     learning it from a Udemy course called: 
+     "Cria jogos para a plataforma que mais cresce no mundo e jogue no Celular, computador ou no Xbox!" 
+     using ChatGPT on the side as my assistant instructor.
+--]]
+
+local userName = "Rodolpho"
+local userNameType = nil
+local revenue = 100
+local expenses = 27
+local balance = revenue - expenses
+
+print(balance)
+
+userNameType = type(userName)
+
+print(userNameType)
